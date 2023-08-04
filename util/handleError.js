05f9016776dfc530ed472b29cbbfa6e833a898ec
Aleksa <office.aleksa.milanovic@gmail.com>
2023-08-04 22:08:28 +0200

    initial commit

--- util/handleError.js
+++ util/handleError.js
@@ -1,6 +1,23 @@
+const ErrorResponse = require(`../util/ErrorResponse`);
+
 const handleError = (res, err) => {
     let error = {...err};
     error.message = err.message;
+
+    if (err.name === 'CastError') {
+        const message = 'Resource not found';
+        error = new ErrorResponse(404, message);
+    }
+
+    if (err.code === 11000) {
+        const message = 'There is a duplicate of inputed value in the database.';
+        error = new ErrorResponse(400, message);
+    }
+
+    if (err.name === 'ValidationError') {
+        const message = Object.values(err.errors).map(value => value.message);
+        error = new ErrorResponse(400, message);
+    }
 
     res.status(error.statusCode || 500).json({
         status: error.statusCode || 500,
